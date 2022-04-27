b06546daed6625354885fe5e0073216b2f938932
ivanCPriego <ivan.cruz.priego@gmail.com>
2022-04-27 00:23:31 -0500

    optimizacion de scripts

--- js/functions.js
+++ js/functions.js
@@ -14,24 +14,19 @@ document.getElementById('arrowright').addEventListener('click', function () {
 });
 
 //MENU ANIMATE
-let mytogglebutton = document.querySelector('.menu_mobile');
-let myhiddenthing = document.querySelector('.box_menu_mobile');
-let mytogglebuttonclose = document.querySelector('.close_manu');
-let myblackback = document.querySelector('.back_modal');
+//tomando elementos por medio del query selector
+const mytogglebutton = document.querySelector('.menu_mobile');
+const myhiddenthing = document.querySelector('.box_menu_mobile');
+const mytogglebuttonclose = document.querySelector('.close_manu');
+const myblackback = document.querySelector('.back_modal');
 
-mytogglebutton.addEventListener('click',function() {
-  showMenu()
-  }
-)
-mytogglebuttonclose.addEventListener('click',function() {
-  showMenu()
-  }
-)
-myblackback.addEventListener('click',function() {
-  showMenu()
-  }
-)
-function showMenu(){
+//funciones que mandan a llamar a la funcion que agregan la clase activ al menu y al fondo
+mytogglebutton.addEventListener('click',()=>showMenu())
+mytogglebuttonclose.addEventListener('click',()=>showMenu())
+myblackback.addEventListener('click',()=>showMenu())
+
+//funcion que agrega la clase activ al menu y al fondo
+const showMenu = ()=>{
   myhiddenthing.classList.toggle('active');
   myblackback.classList.toggle('active');
 }
