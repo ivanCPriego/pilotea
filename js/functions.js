d8ae219df5177b613bb421a3f744a6d0f05b3357
ivanCPriego <ivan.cruz.priego@gmail.com>
2022-04-27 00:12:38 -0500

    agregando animacion a menu mobil

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,37 @@
 // AMIMATE SLIDER - left
 document.getElementById('arrowleft').addEventListener('click', function () {
-  let movement = document.querySelector('.item_slider').offsetWidth
-  document.getElementById('animte_slider').scrollLeft += movement;
+  //calcular el tamaño de un elmento
+  let move = document.querySelector('.item_slider').offsetWidth
+  //desplazar el scroll horizontal respecto a tamaño del elemento  
+  document.getElementById('animte_slider').scrollLeft -= move;
 });
 // AMIMATE SLIDER - right
 document.getElementById('arrowright').addEventListener('click', function () {
-  let movement = document.querySelector('.item_slider').offsetWidth
-  document.getElementById('animte_slider').scrollLeft -= movement;
+  //calcular el tamaño de un elmento
+  let move = document.querySelector('.item_slider').offsetWidth
+  //desplazar el scroll horizontal respecto a tamaño del elemento 
+  document.getElementById('animte_slider').scrollLeft += move;
 });
+
+//MENU ANIMATE
+let mytogglebutton = document.querySelector('.menu_mobile');
+let myhiddenthing = document.querySelector('.box_menu_mobile');
+let mytogglebuttonclose = document.querySelector('.close_manu');
+let myblackback = document.querySelector('.back_modal');
+
+mytogglebutton.addEventListener('click',function() {
+  showMenu()
+  }
+)
+mytogglebuttonclose.addEventListener('click',function() {
+  showMenu()
+  }
+)
+myblackback.addEventListener('click',function() {
+  showMenu()
+  }
+)
+function showMenu(){
+  myhiddenthing.classList.toggle('active');
+  myblackback.classList.toggle('active');
+}
